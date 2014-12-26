13aef557f4fac2f12fd871ea8580618deb2a3085
Benedikt Sauer <filmor@gmail.com>
2014-12-27 00:35:07 +0100

    Make it build again with older rustc version.

--- src/util.rs
+++ src/util.rs
@@ -1,4 +1,4 @@
-use std::slice::Iter;
+use std::slice::Items as Iter;
 
 // A vector type optimized for cases where the size is almost always 0 or 1
 // Code inspired my Mozilla's SmallVector for libsyntax
